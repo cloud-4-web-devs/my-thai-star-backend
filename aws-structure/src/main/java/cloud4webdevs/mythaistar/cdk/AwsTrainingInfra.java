6701457028efce4f234963a69dfc5975985e6d0f
Michal Chrapczynski <michal.chrapczynski@capgemini.com>
2022-04-04 21:39:21 +0200

    Added VPC

--- aws-structure/src/main/java/cloud4webdevs/mythaistar/cdk/AwsTrainingInfra.java
+++ aws-structure/src/main/java/cloud4webdevs/mythaistar/cdk/AwsTrainingInfra.java
@@ -1,11 +1,17 @@
 package cloud4webdevs.mythaistar.cdk;
 
 import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.ec2.Vpc;
 import software.constructs.Construct;
 
 public class AwsTrainingInfra extends Stack {
     public AwsTrainingInfra(final Construct scope, final String name) {
         super(scope, name);
+
+        Vpc.Builder.create(this, "vpc")
+                .vpcName("vpc-awstraining")
+                .maxAzs(3)  // Default is all AZs in region
+                .build();
     }
 }
 
